fde3c6084e9973c8459b1e1f099f918ed0f7bb2f
samanta chaparro <samanta.chaparro87@gmail.com>
2023-04-28 18:32:43 -0300

    subiendo archivo

---
+++ src/udi/tp2/UdiTP2.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udi.tp2;
+import java.util.*;
+/**
+ *
+ * @author SUPCOMERCIO
+ */
+public class UdiTP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+       UdiTP2.Menu();
+       
+    }
+
+    public static void Menu() {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion, v1, v2;
+
+        do {
+            System.out.println("Menu elija una opcion");
+            System.out.println("1- Area triangulo");
+            System.out.println("2- Area Rectangulo");
+            System.out.println("3- Area Circulo");
+            System.out.println("4- Salir");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese la base");
+                    v1 = sc.nextInt();
+                    System.out.println("Ingrese la altura");
+                    v2 = sc.nextInt();
+                    System.out.println("El area del triangulo es de " + UdiTP2.Areatri(v1, v2));
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese la base");
+                    v1 = sc.nextInt();
+                    System.out.println("Ingrese la altura");
+                    v2 = sc.nextInt();
+                    System.out.println("El area del Rectangulo  es de " + UdiTP2.Arearec(v1, v2));
+                    break;
+            }
+        } while (opcion != 4);
+
+    }
+
+    public static int Areatri(int v1, int v2) {
+       
+        int areat = (v1 * v2) / 2;
+        return areat;
+
+    }
+
+    public static int Arearec(int v1, int v2) {
+       int arear = v1 * v2;
+        return arear;
+    }
+
+}
+    
+    
+
